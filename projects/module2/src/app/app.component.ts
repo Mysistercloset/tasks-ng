d88457b0fec5fc2dd98f7f9aa3065a407ccd38c6
Module1 <nuts263@gmail.com>
2020-03-29 17:07:36 +0300

    module2

--- projects/module2/src/app/app.component.ts
+++ projects/module2/src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { IProduct, products } from 'projects/module2/src/mocks/products';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  public products: IProduct[] = products;
+  public chosenProduct!: IProduct;
+
+  public addProduct(chosenProduct: IProduct): void {
+    this.chosenProduct = chosenProduct;
+  }
+
+  ngOnInit() {
+    console.log(products);
+  }
+}
